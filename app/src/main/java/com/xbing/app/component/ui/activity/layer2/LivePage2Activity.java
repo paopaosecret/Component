ae162ec584a61b8dff3829ef9e7290663e9d4fa6
xanderzhao <xanderzhao@tencent.com>
2023-12-05 15:32:53 +0800

    直播滑动，ViewPager2

--- app/src/main/java/com/xbing/app/component/ui/activity/layer2/LivePage2Activity.java
+++ app/src/main/java/com/xbing/app/component/ui/activity/layer2/LivePage2Activity.java
@@ -4,13 +4,46 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.xbing.app.component.R;
+import com.xbing.app.component.bean.LiveRoomInfo;
+import com.xbing.app.component.ui.adapter.live.viewpager.VerticalPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LivePage2Activity extends AppCompatActivity {
+    private   ViewPager2         mViewPager;
+    protected List<LiveRoomInfo> mVideos = new ArrayList<>();
+    protected VerticalPagerAdapter mAdapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_pager2);
+
+        initView();
+        initData();
+    }
+
+    private void initView() {
+        mViewPager = findViewById(R.id.view_pager2);
+        mAdapter = new VerticalPagerAdapter(mVideos);
+        mViewPager.setAdapter(mAdapter);
+
+        mViewPager.setOrientation(ViewPager2.ORIENTATION_VERTICAL);
+    }
+
+    private void initData() {
+        List<LiveRoomInfo> roomList = new ArrayList<>();
+        roomList.add(new LiveRoomInfo("1", 0xFFFF0000));
+        roomList.add(new LiveRoomInfo("2", 0xFF00FF00));
+        roomList.add(new LiveRoomInfo("3", 0xFF0000FF));
+        roomList.add(new LiveRoomInfo("4", 0xFFFFFF00));
+        roomList.add(new LiveRoomInfo("5", 0xFFFF00FF));
+        roomList.add(new LiveRoomInfo("6", 0xFF00FFFF));
+        mVideos.addAll(roomList);
+        mAdapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/com/xbing/app/component/ui/adapter/live/viewpager/VerticalPagerAdapter.java
@@ -0,0 +1,58 @@
+package com.xbing.app.component.ui.adapter.live.viewpager;
+
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.xbing.app.component.R;
+import com.xbing.app.component.bean.LiveRoomInfo;
+
+import java.util.List;
+
+public class VerticalPagerAdapter extends RecyclerView.Adapter<VerticalPagerAdapter.ViewHolder> {
+
+    private List<LiveRoomInfo> roomList;
+
+    public VerticalPagerAdapter(List<LiveRoomInfo> list) {
+        this.roomList = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_item_live2, parent, false);
+        TextView textView = new TextView(parent.getContext());
+        textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        textView.setGravity(Gravity.CENTER);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.mTextTitle.setText(roomList.get(position).roomId);
+        holder.mImageBackground.setBackgroundColor(roomList.get(position).backgroundColor);
+    }
+
+    @Override
+    public int getItemCount() {
+        return roomList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public int mPosition;
+        public TextView  mTextTitle;
+        public ImageView mImageBackground;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mTextTitle = itemView.findViewById(R.id.tv_title);
+            mImageBackground = itemView.findViewById(R.id.iv_background);
+        }
+    }
+}
